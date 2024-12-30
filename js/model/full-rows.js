cde7043efc905c32fcb1b663ef58ed9eae47a374
UniBreakfast <unibreakfast1@gmail.com>
2024-12-30 21:04:41 +0200

    fix new rows

--- js/model/full-rows.js
+++ js/model/full-rows.js
@@ -1,10 +1,10 @@
 export { removeFullRows }
 
-import { rowCount } from "../../config.js"
+import { rowCount,columnCount } from "../../config.js"
 import { getEmptyGrid } from "./grid.js"
 
 function removeFullRows(frozen) {
   const leftovers = frozen.filter(row => !row.every(Boolean))
-  const newRows = getEmptyGrid(20 - leftovers.length)
+  const newRows = getEmptyGrid(20 - leftovers.length, columnCount)
   frozen.splice(0, rowCount, ...newRows, ...leftovers)
 }
